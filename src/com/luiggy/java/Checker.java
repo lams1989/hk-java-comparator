bf3f26960b73bc55e3527fc073a1b75820e2d112
luis sierra <luisalejandromunozsierra@gmail.com>
2020-06-19 19:32:54 -0500

    first commit

---
+++ src/com/luiggy/java/Checker.java
@@ -0,0 +1,18 @@
+package com.luiggy.java;
+
+import java.util.Comparator;
+
+public class Checker implements Comparator<Player> {
+
+	@Override
+	public int compare(Player o1, Player o2) {
+		if (o1.score < o2.score) {
+			return 1;
+		}else if (o1.score > o2.score) {
+			return -1;
+		}else {
+			return o1.name.compareTo(o2.name);	
+		}
+	}
+ 
+}
